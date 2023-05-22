df99f605b6d0867890046c62ea376827d9a3b780
MarcelloDeidda <marcello35@hotmail.it>
2023-05-22 21:57:37 +0100

    Removed catchAsync function, updated ExpressError

--- utils/catch-async.js
+++
@@ -1,6 +0,0 @@
-// catchAsync middleware catches async errors to be handled
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
--- utils/express-error.js
+++ utils/express-error.js
@@ -1,8 +1,7 @@
 // ExpressError class contains an error message and a statusCode
 class ExpressError extends Error {
     constructor(message, statusCode) {
-        super();
-        this.message = message;
+        super(message);
         this.statusCode = statusCode;
     }
 }
