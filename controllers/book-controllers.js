ab50772c7488549b33cbf6cc74b2a89d71aece5f
MarcelloDeidda <marcello35@hotmail.it>
2023-05-22 22:54:53 +0100

    Added createBook controller and route, with appropriate validation

--- controllers/book-controllers.js
+++ controllers/book-controllers.js
@@ -5,6 +5,7 @@ const User = require("../models/book");
 // Importing utils
 const categories = require("../utils/categories");
 const ExpressError = require("../utils/express-error");
+const { validateCategory, checkValidation } = require("../utils/validation");
 
 module.exports.getAllBooks = async (req, res) => {
     let books;
@@ -15,4 +16,31 @@ module.exports.getAllBooks = async (req, res) => {
     }
 
     res.status(200).json({ books: books.map(book => book.toObject({ getters: true })) });
+}
+
+module.exports.createBook = async (req, res, next) => {
+    checkValidation(req, next);
+    
+    const { title, author, year, summary, imgUrl, category } = req.body;
+
+    validateCategory(category, next);
+
+    const book = new Book({
+        title,
+        author,
+        year,
+        summary,
+        imgUrl,
+        category,
+        createdBy: "111",
+        reviews: []
+    });
+
+    try {
+        await book.save();
+    } catch (e) {
+        return next(new ExpressError(e.message, 500));
+    }
+
+    res.status(201).json({ message: "Book was successfully created!", book: book.toObject({ getters: true }) });
 }
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
 const bookControllers = require("../controllers/book-controllers");
+const validation = require("../utils/validation");
 
 router.get("/", bookControllers.getAllBooks);
+router.post("/", validation.createBookValidator(), bookControllers.createBook);
 
 module.exports = router;
