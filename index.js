7d5fd5542f08026e83132f2dcc13fcae8f0bb214
MarcelloDeidda <marcello35@hotmail.it>
2023-10-17 16:33:16 +0100

    Status code bug fix

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
     if (res.headerSent) {
         return next(err);
     }
-    res.status(err.code || 500).json({message: err.message || "An unknown error occurred!"});
+    res.status(err.statusCode || 500).json({message: err.message || "An unknown error occurred!"});
 });
 
 // Connecting database and start listening
